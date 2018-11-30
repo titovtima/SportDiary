b3320833141b3c96b9ff57ffdafbbeef54bdc9d9
Evgeniy <j.borodin1@mail.ru>
2018-11-30 19:03:18 +0300

    List

--- app/src/main/java/ru/evgeniiborodin/deniskorotchenko/sportdiary/MainActivity.kt
+++ app/src/main/java/ru/evgeniiborodin/deniskorotchenko/sportdiary/MainActivity.kt
@@ -3,7 +3,12 @@ package ru.evgeniiborodin.deniskorotchenko.sportdiary
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.ArrayAdapter
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +19,24 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AddActivity::class.java)
             startActivity(intent)
         }
+        // supportActionBar!!.hide()
+        supportActionBar!!.title = Date().toString()
+
+        val listView = findViewById<View>(R.id.listView) as ListView
+
+        val sportActivity = arrayOf(
+            "Бег",
+            "Отжимания",
+            "Присидания",
+            "Подтягивания"
+        )
+
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1, sportActivity
+        )
+
+        listView.adapter = adapter
     }
 
 }
