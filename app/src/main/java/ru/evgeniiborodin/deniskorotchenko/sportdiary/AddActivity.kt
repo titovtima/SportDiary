d8089d72e7e48d9cee20e65eca1603370aa020b0
DenisKorotchenko <dzd1024ozd@gmail.com>
2018-11-30 17:15:38 +0300

    AddActivity added

---
+++ app/src/main/java/ru/evgeniiborodin/deniskorotchenko/sportdiary/AddActivity.kt
@@ -0,0 +1,12 @@
+package ru.evgeniiborodin.deniskorotchenko.sportdiary
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add)
+    }
+}
