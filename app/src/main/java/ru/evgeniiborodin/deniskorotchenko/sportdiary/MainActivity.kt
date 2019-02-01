a09b5e7ee378c95ab4b2ed053680f689de37be74
Evgeniy <j.borodin1@mail.ru>
2019-02-01 19:15:43 +0300

    fragments

--- app/src/main/java/ru/evgeniiborodin/deniskorotchenko/sportdiary/MainActivity.kt
+++ app/src/main/java/ru/evgeniiborodin/deniskorotchenko/sportdiary/MainActivity.kt
@@ -3,10 +3,13 @@ package ru.evgeniiborodin.deniskorotchenko.sportdiary
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import ru.evgeniiborodin.deniskorotchenko.sportdiary.Fragments.Exercises
+import ru.evgeniiborodin.deniskorotchenko.sportdiary.Fragments.Statistics
 
 
 class MainActivity : AppCompatActivity() {
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,22 +20,25 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
-                .add(R.id.exercises,Exercises())
+                .add(R.id.container,Exercises.newInstance())
+                .addToBackStack(null)
                 .commit()
         }
     }
 
-    /*fun onExercises(view: View) {
-        transaction = fragmentManager.beginTransaction()
-        transaction.replace(R.id.container, exercises)
-        transaction.commit()
+    fun onExercises(view: View) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container,Exercises.newInstance())
+            .commit()
     }
 
     fun onStatistics(view: View) {
-        transaction = fragmentManager.beginTransaction()
-        transaction.replace(R.id.container, exercises)
-        transaction.commit()
-    }*/
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container,Statistics.newInstance())
+            .commit()
+    }
 
 
 }
