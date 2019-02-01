b5090cd027af5a4f301d3319b32a0d206715b2e3
Evgeniy <j.borodin1@mail.ru>
2019-02-01 16:44:59 +0300

    sm changes

--- app/src/main/java/ru/evgeniiborodin/deniskorotchenko/sportdiary/Fragments/Exercises.kt
+++ app/src/main/java/ru/evgeniiborodin/deniskorotchenko/sportdiary/Fragments/Exercises.kt
@@ -6,25 +6,23 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import ru.evgeniiborodin.deniskorotchenko.sportdiary.R
 
+
+//1
 class Exercises : Fragment() {
 
+    //2
+    companion object {
 
+        fun newInstance(): Exercises {
+            return Exercises()
+        }
+    }
 
-
+    //3
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
-        val productsView = inflater.inflate(R.layout.exercises, container, false)
-        return productsView
+        return inflater.inflate(R.layout.exercises, container, false)
     }
 
-    fun onButton(view: View) {
-        val toast = Toast.makeText(
-            activity!!.applicationContext,
-            "Заработало", Toast.LENGTH_SHORT
-        )
-        toast.show()
-    }
 }
--- app/src/main/java/ru/evgeniiborodin/deniskorotchenko/sportdiary/MainActivity.kt
+++ app/src/main/java/ru/evgeniiborodin/deniskorotchenko/sportdiary/MainActivity.kt
@@ -3,30 +3,26 @@ package ru.evgeniiborodin.deniskorotchenko.sportdiary
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import ru.evgeniiborodin.deniskorotchenko.sportdiary.Fragments.Exercises
-import ru.evgeniiborodin.deniskorotchenko.sportdiary.Fragments.Statistics
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var exercises: Exercises
-    private lateinit var statistics: Statistics
-    private lateinit var transaction: android.app.FragmentTransaction
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setContentView(R.layout.activity_main)
         super.onCreate(savedInstanceState)
-        exercises = Exercises()
-        statistics = Statistics()
+        setContentView(R.layout.activity_main)
 
 
-        transaction = fragmentManager.beginTransaction()
-        transaction.replace(R.id.container, exercises)
-        transaction.commit()
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.exercises,Exercises())
+                .commit()
+        }
     }
 
-    fun onExercises(view: View) {
+    /*fun onExercises(view: View) {
         transaction = fragmentManager.beginTransaction()
         transaction.replace(R.id.container, exercises)
         transaction.commit()
@@ -34,9 +30,9 @@ class MainActivity : AppCompatActivity() {
 
     fun onStatistics(view: View) {
         transaction = fragmentManager.beginTransaction()
-        transaction.replace(R.id.container, statistics)
+        transaction.replace(R.id.container, exercises)
         transaction.commit()
-    }
+    }*/
 
 
 }
