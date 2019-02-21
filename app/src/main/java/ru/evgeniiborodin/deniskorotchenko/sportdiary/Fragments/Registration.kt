010396541578fec69c0a40088dde7b6679777c34
Evgeniy <j.borodin1@mail.ru>
2019-02-21 23:39:18 +0300

    Registration is working

--- app/src/main/java/ru/evgeniiborodin/deniskorotchenko/sportdiary/Fragments/Registration.kt
+++ app/src/main/java/ru/evgeniiborodin/deniskorotchenko/sportdiary/Fragments/Registration.kt
@@ -5,20 +5,24 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 import ru.evgeniiborodin.deniskorotchenko.sportdiary.R
 
-class Statistics : Fragment() {
+class Registration : Fragment() {
 
     companion object {
 
-        fun newInstance(): Statistics {
-            return Statistics()
+        fun newInstance(): Registration {
+            return Registration()
         }
     }
 
     //3
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.statistics, container, false)
+        return inflater.inflate(R.layout.reg, container, false)
     }
 
+
 }
+
